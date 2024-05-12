f59437552470bc92ab5b9bc2a8f643a5e7d46a6e
fw-real <nostorian@proton.me>
2024-05-13 04:21:30 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,9 @@
+CREATE DATABASE IF NOT EXISTS crushai_db;
+
+USE crushai_db;
+
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(100),
-    chat_id VARCHAR(100)
+    chat_id VARCHAR(50)
 );
